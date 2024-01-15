3603d9a1980206a18342bc2b649e6647081eea32
Алексей Шлапаков <theblack2swan@gmail.com>
2024-01-15 16:35:36 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world six!");
         System.out.println("hello seven");
-
+        System.out.println("hello eight");
     }
 }
